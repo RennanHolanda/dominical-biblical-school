fc521e5ff6b4a2d7b8bf78b99a50f48ce1424159
Bruno Rennan <bruno_rennan@hotmail.com>
2024-05-29 21:31:55 -0300

    create controller teacher

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import config from "config";
 import admRoutes from "./routes/admRoutes"
+import teacherRoutes from "./routes/teacherRoutes"
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/", admRoutes);
+app.use("/api/", teacherRoutes);
 
 const port = config.get<number>("port")
 
---
+++ src/routes/teacherRoutes.ts
@@ -0,0 +1,43 @@
+import { Router, Request, Response } from "express";
+import { TeacherController } from "../controllers/teachersController";
+
+const router = Router();
+const teacherController = new TeacherController();
+
+router.get("/teacher/index", (req: Request, res: Response) => {
+    const message = teacherController.index();
+
+    res.send(message);
+})
+
+router.get("/teacher/register", (req: Request, res: Response) => {
+    const message = teacherController.showRegister();
+
+    res.send(message);
+})
+
+router.post("/teacher/register", (req: Request, res: Response) => {
+    const message = teacherController.register();
+
+    res.send(message);
+})
+
+router.get("/teacher/:id/edit", (req: Request, res: Response) => {
+    const message = teacherController.showEdit();
+
+    res.send(message);
+})
+
+router.post("/teacher/:id/edit", (req: Request, res: Response) => {
+    const message = teacherController.update();
+
+    res.send(message);
+})
+
+router.get("/teacher/:id", (req: Request, res: Response) => {
+    const message = teacherController.delete();
+
+    res.send(message);
+})
+
+export default router;
